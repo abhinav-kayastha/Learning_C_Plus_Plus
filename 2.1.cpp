c9cebe2393f51a182ecb18e22ded5aed3b65c134
abhinav-kayastha <Abhinav.Kayastha@metropolia.fi>
2024-01-21 18:20:29 +0200

    finished 2.1.cpp

--- 2.1.cpp
+++ 2.1.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "iostream"
+#include "sstream"
 
 using namespace std;
 
@@ -10,8 +11,27 @@ int main() {
 
     string decision;
 
-    while (true) {
 
+    while (true) {
+        cout << "Enter a line with numbers or 'stop': ";
+        getline(cin, decision);
+        if (decision == "stop")
+        {
+            cout << "Program Stopped." << endl;
+            break;
+        }
+
+        istringstream iss(decision);
+        int number;
+        int sum = 0;
+        int count = 0;
+
+        while (iss >> number) {
+            sum += number;
+            count++;
+        }
+
+        cout << "Total of " << count << " numbers is " << sum << endl;
     }
 
 }
