548515619f2f9ac3694cec98391ad2e43e340941
abhinav-kayastha <Abhinav.Kayastha@metropolia.fi>
2024-01-23 14:12:48 +0200

    finished 3.1

--- 3.1.cpp
+++ 3.1.cpp
@@ -15,14 +15,16 @@ int main() {
     // inserts key value pairs using make_pair func.: nameNum.insert(make_pair("yeet", 1));
 
     while (true) {
-        cout << "Enter name or “stop” or “print”: ";
+        cout << "Enter name or 'stop' or 'print': " << endl;
         getline(cin, decision);
 
         if (decision == "stop") {
+            cout << "Stopping program.";
             break;
         }
 
         else if (decision == "print") {
+            it = nameNum.begin();
             while (it != nameNum.end()) {
                 cout << it->first << " : " << it->second << endl;
                 it++;
@@ -34,9 +36,7 @@ int main() {
             cout << "Enter integer: ";
             cin >> num;
             nameNum.insert(make_pair(decision, num));
+            cin.ignore();
         }
     }
-
-
-
 }
