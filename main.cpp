8a58bc9b2b67b4fc65e643e3565817e695da3c3c
abhinav-kayastha <Abhinav.Kayastha@metropolia.fi>
2024-05-05 15:35:54 +0300

    changing function types to return floating nums

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <string>
+
+float add(float a, float b) {
+    return a + b;
+}
+
+float subtract(float a, float b) {
+    return a - b;
+}
+
+float multiply(float a, float b) {
+    return a * b;
+}
+
+float divide(float a, float b) {
+    return a / b;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    while (true) {
+        float num1 = 0;
+        float num2 = 0;
+        std::string choice;
+
+        std::cout << "Enter operation type (+, -, *, /, q): ";
+        std::cin >> choice;
+
+        std::cout << "Enter first number: ";
+        std::cin >> num1;
+        std::cout << "Enter second number: ";
+        std::cin >> num2;
+
+        if (choice == "+") {
+            std::cout << "Result: " << add(num1, num2) << std::endl;
+        } else if (choice == "-") {
+            std::cout << "Result: " << subtract(num1, num2) << std::endl;
+        } else if (choice == "*") {
+            std::cout << "Result: " << multiply(num1, num2) << std::endl;
+        } else if (choice == "/") {
+            std::cout << "Result: " << divide(num1, num2) << std::endl;
+        } else if (choice == "q") {
+            return 0;
+        } else {
+            std::cout << "Invalid operation type" << std::endl;
+        }
+    }
     return 0;
 }
